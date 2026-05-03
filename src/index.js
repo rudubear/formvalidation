75a387bb9d55c0bb802b54296d006dec35499705
RuduBear <134881527+rudubear@users.noreply.github.com>
2026-05-03 11:38:30 -0400

    drawing the rest of the owl

--- src/index.js
+++ src/index.js
@@ -1,47 +1,271 @@
 // index.js
 import "./styles.css";
 
-import img_restaurant_bg from "../assets/images/restaurant_background.png";
+import * as DOMRenderer from "./renderHTML.js";
 
-import { page_load_text_home, div_home } from "./home.js";
-import { page_load_text_menu, div_menu} from "./menu.js";
-import { page_load_text_about, div_aboutus } from "./about.js";
-import { page_HTMLRenderer } from "./renderHTML.js";
+let divBody = document.getElementById("bodyContent");
 
-console.log(page_load_text_about);
-console.log(page_load_text_home);
-console.log(page_load_text_menu);
-console.log(page_HTMLRenderer);
+const inputFieldMinLength = 3;
+const inputFieldMaxLength = 20;
 
-let divContent = document.getElementById("content");
-let divBody = document.querySelector('body');
+const HTMLInputName = {
+  id: "myName",
+  defaultValue: "Name",
+  type: "text",
+  isReadOnly: false,
+  isRequired: true,
+  invalidInputMsg: "invalid input",
+  RegexpPattern: undefined,
+  minLength: 3,
+  maxLength: undefined,
+  minNumerical: undefined,
+  maxNumerical: undefined,
+};
 
-divContent.appendChild(div_home);
+const HTMLInputEmail = {
+  id: "myEmail",
+  defaultValue: "email",
+  type: "email",
+  isReadOnly: false,
+  isRequired: true,
+  invalidInputMsg: "invalid email",
+  RegexpPattern: undefined,
+  minLength: undefined,
+  maxLength: undefined,
+  minNumerical: undefined,
+  maxNumerical: undefined,
+};
 
-const image_restaurant_bg = document.createElement("img");
-image_restaurant_bg.src = img_restaurant_bg;
-divBody.style.backgroundImage = `url(${img_restaurant_bg})`;
+const HTMLInputCountry = {
+  id: "myCountry",
+  defaultValue: "Country",
+  type: "text",
+  isReadOnly: false,
+  isRequired: true,
+  invalidInputMsg: "invalid country",
+  RegexpPattern: undefined,
+  minLength: 3,
+  maxLength: undefined,
+  minNumerical: undefined,
+  maxNumerical: undefined,
+};
 
+const HTMLInputPostalCode = {
+  id: "myPostalCode",
+  defaultValue: "Postal Code",
+  type: "text",
+  isReadOnly: false,
+  isRequired: true,
+  invalidInputMsg: "invalid postal code",
+  RegexpPattern:
+    "[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z][ ]?[0-9][ABCEGHJ-NPRSTV-Z][0-9]",
+  minLength: 3,
+  maxLength: undefined,
+  minNumerical: undefined,
+  maxNumerical: undefined,
+};
 
-const btn_home = document.getElementById("btn_Home");
-const btn_menu = document.getElementById("btn_Menu");
-const btn_about = document.getElementById("btn_About");
+const HTMLInputPassword1 = {
+  id: "myPassword1",
+  defaultValue: undefined,
+  type: "password",
+  isReadOnly: false,
+  isRequired: true,
+  invalidInputMsg: undefined,
+  RegexpPattern: undefined,
+  minLength: 3,
+  maxLength: undefined,
+  minNumerical: undefined,
+  maxNumerical: undefined,
+};
 
-btn_home.addEventListener("click", (event) => {
-    console.log("home button clicked");
-    divContent.replaceChildren();
-    divContent.appendChild(div_home);
+const HTMLInputPassword2 = {
+  id: "myPassword2",
+  defaultValue: undefined,
+  type: "password",
+  isReadOnly: false,
+  isRequired: true,
+  invalidInputMsg: undefined,
+  RegexpPattern: undefined,
+  minLength: 3,
+  maxLength: undefined,
+  minNumerical: undefined,
+  maxNumerical: undefined,
+};
 
-});
+let myInputName = DOMRenderer.createHTMLelement_InputText(
+  HTMLInputName.id,
+  HTMLInputName.defaultValue,
+  HTMLInputName.type,
+  HTMLInputName.isReadOnly,
+  HTMLInputName.isRequired,
+  HTMLInputName.invalidInputMsg,
+  HTMLInputName.RegexpPattern,
+  HTMLInputName.minLength,
+  HTMLInputName.maxLength,
+  HTMLInputName.minNumerical,
+  HTMLInputName.maxNumerical
+);
 
-btn_menu.addEventListener("click", (event) => {
-    console.log("menu button clicked");
-    divContent.replaceChildren();
-    divContent.appendChild(div_menu);
-});
+let myInputEmail = DOMRenderer.createHTMLelement_InputText(
+  HTMLInputEmail.id,
+  HTMLInputEmail.defaultValue,
+  HTMLInputEmail.type,
+  HTMLInputEmail.isReadOnly,
+  HTMLInputEmail.isRequired,
+  HTMLInputEmail.invalidInputMsg,
+  HTMLInputEmail.RegexpPattern,
+  HTMLInputEmail.minLength,
+  HTMLInputEmail.maxLength,
+  HTMLInputEmail.minNumerical,
+  HTMLInputEmail.maxNumerical
+);
 
-btn_about.addEventListener("click", (event) => {
-    console.log("about button clicked");
-    divContent.replaceChildren();
-    divContent.appendChild(div_aboutus);
-});
+let myInputCountry = DOMRenderer.createHTMLelement_InputText(
+  HTMLInputCountry.id,
+  HTMLInputCountry.defaultValue,
+  HTMLInputCountry.type,
+  HTMLInputCountry.isReadOnly,
+  HTMLInputCountry.isRequired,
+  HTMLInputCountry.invalidInputMsg,
+  HTMLInputCountry.RegexpPattern,
+  HTMLInputCountry.minLength,
+  HTMLInputCountry.maxLength,
+  HTMLInputCountry.minNumerical,
+  HTMLInputCountry.maxNumerical
+);
+
+let myInputPostalCode = DOMRenderer.createHTMLelement_InputText(
+  HTMLInputPostalCode.id,
+  HTMLInputPostalCode.defaultValue,
+  HTMLInputPostalCode.type,
+  HTMLInputPostalCode.isReadOnly,
+  HTMLInputPostalCode.isRequired,
+  HTMLInputPostalCode.invalidInputMsg,
+  HTMLInputPostalCode.RegexpPattern,
+  HTMLInputPostalCode.minLength,
+  HTMLInputPostalCode.maxLength,
+  HTMLInputPostalCode.minNumerical,
+  HTMLInputPostalCode.maxNumerical
+);
+
+let myInputPassword1 = DOMRenderer.createHTMLelement_InputText(
+  HTMLInputPassword1.id,
+  HTMLInputPassword1.defaultValue,
+  HTMLInputPassword1.type,
+  HTMLInputPassword1.isReadOnly,
+  HTMLInputPassword1.isRequired,
+  HTMLInputPassword1.invalidInputMsg,
+  HTMLInputPassword1.RegexpPattern,
+  HTMLInputPassword1.minLength,
+  HTMLInputPassword1.maxLength,
+  HTMLInputPassword1.minNumerical,
+  HTMLInputPassword1.maxNumerical
+);
+
+let myInputPassword2 = DOMRenderer.createHTMLelement_InputText(
+  HTMLInputPassword2.id,
+  HTMLInputPassword2.defaultValue,
+  HTMLInputPassword2.type,
+  HTMLInputPassword2.isReadOnly,
+  HTMLInputPassword2.isRequired,
+  HTMLInputPassword2.invalidInputMsg,
+  HTMLInputPassword2.RegexpPattern,
+  HTMLInputPassword2.minLength,
+  HTMLInputPassword2.maxLength,
+  HTMLInputPassword2.minNumerical,
+  HTMLInputPassword2.maxNumerical
+);
+
+let myLabelName = DOMRenderer.createHTMLelement_label("Name :");
+let mylabelEmail = DOMRenderer.createHTMLelement_label("Email :");
+let mylabelCountry = DOMRenderer.createHTMLelement_label("Country :");
+let mylabelPostalCode = DOMRenderer.createHTMLelement_label("Postal Code :");
+let mylabelPassword1 = DOMRenderer.createHTMLelement_label("Password :");
+let mylabelPassword2 = DOMRenderer.createHTMLelement_label("Confirm PW :");
+
+let myfn = (e) => {
+  e.preventDefault();
+  console.log("submitted, override default behaviour");
+  let myFilledForm = document.getElementById("myForm");
+  let pw1 = document.getElementById(HTMLInputPassword1.id);
+  let pw2 = document.getElementById(HTMLInputPassword2.id);
+
+  if (pw1.value === pw2.value) {
+    pw2.setCustomValidity("");
+  } else {
+    pw2.setCustomValidity("Passwords do not match");
+  }
+
+  if (myFilledForm.checkValidity()) {
+    console.log("all input is good");
+  } else {
+    console.log("error on form");
+  }
+  myFilledForm.reportValidity();
+};
+
+let myFormSubmitBtn = DOMRenderer.createHTMLelement_Button("submit", myfn);
+myFormSubmitBtn.type = "submit";
+
+let myForm = DOMRenderer.createHTMLelement_Form("myForm");
+myForm.noValidate = true;
+
+let myTable = DOMRenderer.createHTMLelement_TABLE();
+let myTableBody = DOMRenderer.createHTMLelement_TableBody();
+let myTableCaptions =
+  DOMRenderer.createHTMLelement_TableCaption("submission form");
+let myTableHeaders = DOMRenderer.createHTMLelement_TableHeaders([
+  "description",
+  "input field",
+]);
+
+let myTableRow1 = DOMRenderer.createHTMLelement_TableRowWithElements([
+  myLabelName,
+  myInputName,
+]);
+let myTableRow2 = DOMRenderer.createHTMLelement_TableRowWithElements([
+  mylabelEmail,
+  myInputEmail,
+]);
+let myTableRow3 = DOMRenderer.createHTMLelement_TableRowWithElements([
+  mylabelCountry,
+  myInputCountry,
+]);
+let myTableRow4 = DOMRenderer.createHTMLelement_TableRowWithElements([
+  mylabelPostalCode,
+  myInputPostalCode,
+]);
+let myTableRow5 = DOMRenderer.createHTMLelement_TableRowWithElements([
+  mylabelPassword1,
+  myInputPassword1,
+]);
+let myTableRow6 = DOMRenderer.createHTMLelement_TableRowWithElements([
+  mylabelPassword2,
+  myInputPassword2,
+]);
+let myTableRow7 = DOMRenderer.createHTMLelement_TableRowWithElements([
+  myFormSubmitBtn,
+]);
+
+myTableBody.appendChild(myTableRow1);
+myTableBody.appendChild(myTableRow2);
+myTableBody.appendChild(myTableRow3);
+myTableBody.appendChild(myTableRow4);
+myTableBody.appendChild(myTableRow5);
+myTableBody.appendChild(myTableRow6);
+myTableBody.appendChild(myTableRow7);
+
+myTable.appendChild(myTableCaptions);
+myTable.appendChild(myTableHeaders);
+myTable.appendChild(myTableBody);
+
+myForm.appendChild(myTable);
+//myForm.appendChild(myInputEmail);
+//myForm.appendChild(myInputCountry);
+//myForm.appendChild(myInputPostalCode);
+//myForm.appendChild(myInputPassword1);
+//myForm.appendChild(myInputPassword2);
+//myForm.appendChild(myFormSubmitBtn);
+
+divBody.appendChild(myForm);
--- src/renderHTML.js
+++ src/renderHTML.js
@@ -1,109 +1,222 @@
-const page_HTMLRenderer = "loading HTML Renderer!"
+const page_HTMLRenderer = "loading HTML Renderer!";
 
-function renderHTMLMenuObject (menuItem, menuItemImageSource){
-    let listItem = document.createElement("li");
+function renderHTMLMenuObject(menuItem, menuItemImageSource) {
+  let listItem = document.createElement("li");
 
-    let menuItemH2 = document.createElement("h2");
-    let menuItemDescription = document.createElement("p");
-    let menuItemPrice = document.createElement("p");
-    let menuItemImage = new Image();
-    menuItemImage.src = menuItemImageSource;
-    menuItemImage.classList = "menu_item_image";    
-    
-    
-    menuItemH2.textContent = menuItem.name;
-    menuItemDescription.textContent = menuItem.description;
-    menuItemPrice.textContent = menuItem.price;
+  let menuItemH2 = document.createElement("h2");
+  let menuItemDescription = document.createElement("p");
+  let menuItemPrice = document.createElement("p");
+  let menuItemImage = new Image();
+  menuItemImage.src = menuItemImageSource;
+  menuItemImage.classList = "menu_item_image";
 
-    let myMenuItem = [];
-    myMenuItem.push(menuItemH2);
-    myMenuItem.push(menuItemDescription);
-    myMenuItem.push(menuItemPrice);
-    myMenuItem.push(menuItemImage);
+  menuItemH2.textContent = menuItem.name;
+  menuItemDescription.textContent = menuItem.description;
+  menuItemPrice.textContent = menuItem.price;
 
-    listItem.appendChild(menuItemH2);
-    listItem.appendChild(menuItemDescription);
-    listItem.appendChild(menuItemPrice);
-    listItem.appendChild(menuItemImage);
+  let myMenuItem = [];
+  myMenuItem.push(menuItemH2);
+  myMenuItem.push(menuItemDescription);
+  myMenuItem.push(menuItemPrice);
+  myMenuItem.push(menuItemImage);
 
-    return listItem ;
+  listItem.appendChild(menuItemH2);
+  listItem.appendChild(menuItemDescription);
+  listItem.appendChild(menuItemPrice);
+  listItem.appendChild(menuItemImage);
 
+  return listItem;
 }
 
-function createHTMLelement_P(text){
-    let myP = document.createElement("p");
-    myP.innerText = text;
-    
-    return myP;
+function createHTMLelement_P(text) {
+  let myP = document.createElement("p");
+  myP.innerText = text;
+
+  return myP;
 }
 
-function createHTMLelement_H(text){
-    let myH1 = document.createElement("h1");
-    myH1.innerText = text;
-    
-    return myH1;
+function createHTMLelement_label(text) {
+  let myLabel = document.createElement("label");
+  myLabel.textContent = text;
+
+  return myLabel;
 }
 
-function createHTMLelement_DIV(styleclass){
-    let myDIV = document.createElement("div");
-    if (styleclass) {
-        myDIV.classList = styleclass;
+function createHTMLelement_H(text) {
+  let myH1 = document.createElement("h1");
+  myH1.innerText = text;
+
+  return myH1;
+}
+
+function createHTMLelement_DIV(styleclass) {
+  let myDIV = document.createElement("div");
+  if (styleclass) {
+    myDIV.classList = styleclass;
+  }
+  return myDIV;
+}
+
+function createHTMLelement_TABLE(styleclass) {
+  let myTable = document.createElement("table");
+  if (styleclass) {
+    myTable.classList = styleclass;
+  }
+
+  return myTable;
+}
+
+function createHTMLelement_TableCaption(captionText) {
+  let myCaption = document.createElement("caption");
+  myCaption.innerText = captionText;
+  return myCaption;
+}
+
+function createHTMLelement_TableHeaders(array_of_headers = []) {
+  let myTableHead = document.createElement("thead");
+  let myTableHeadRow = document.createElement("tr");
+  array_of_headers.forEach((header) => {
+    let myTableHeadRowHeader = document.createElement("th");
+    myTableHeadRowHeader.innerText = header;
+    myTableHeadRow.appendChild(myTableHeadRowHeader);
+  });
+  myTableHead.appendChild(myTableHeadRow);
+
+  return myTableHead;
+}
+
+function createHTMLelement_TableBody() {
+  let myTableBody = document.createElement("tbody");
+  return myTableBody;
+}
+
+function createHTMLelement_TableRow(array_of_data) {
+  let myTableBodyRow = document.createElement("tr");
+  array_of_data.forEach((data) => {
+    let myTableBodyRowData = document.createElement("td");
+    myTableBodyRowData.innerText = data;
+    myTableBodyRow.appendChild(myTableBodyRowData);
+  });
+
+  return myTableBodyRow;
+}
+
+function createHTMLelement_TableRowWithElements(array_of_elements) {
+  let myTableBodyRow = document.createElement("tr");
+  array_of_elements.forEach((element) => {
+    let myTableBodyRowData = document.createElement("td");
+    myTableBodyRowData.appendChild(element);
+    myTableBodyRow.appendChild(myTableBodyRowData);
+  });
+
+  return myTableBodyRow;
+}
+
+function createHTMLelement_Form(id) {
+  let myForm = document.createElement("form");
+  myForm.setAttribute("id", id);
+
+  return myForm;
+}
+
+function createHTMLelement_InputText(
+  elementID,
+  elementDefaultValue = undefined,
+  elementType = undefined,
+  elementReadOnly = false,
+  elementRequired = false,
+  invalidInputMsg = "",
+  elementRegexpPattern = undefined,
+  elementMinLength = undefined,
+  elementMaxLength = undefined,
+  elementMinNumerical = undefined,
+  elementMaxNumerical = undefined
+) {
+  let myInputField = document.createElement("input");
+
+  myInputField.setAttribute("id", elementID);
+
+  if (elementDefaultValue) {
+    myInputField.value = elementDefaultValue;
+  }
+
+  if (elementType) {
+    myInputField.type = elementType;
+  }
+
+  if (elementReadOnly) {
+    myInputField.readOnly = elementReadOnly;
+  }
+
+  if (elementRequired) {
+    myInputField.required = elementRequired;
+  }
+
+  if (elementRegexpPattern) {
+    myInputField.pattern = elementRegexpPattern;
+  }
+
+  if (elementMinLength) {
+    myInputField.minLength = elementMinLength;
+  }
+
+  if (elementMaxLength) {
+    myInputField.maxLength = elementMaxLength;
+  }
+
+  if (elementMinNumerical) {
+    myInputField.min = elementMinNumerical;
+  }
+
+  if (elementMaxNumerical) {
+    myInputField.max = elementMaxNumerical;
+  }
+
+  myInputField.addEventListener("input", () => {
+    if (myInputField.checkValidity()) {
+      console.log("valid input entered for " + elementID) + invalidInputMsg;
+    } else {
+      console.log("invalid input entered for " + elementID + invalidInputMsg);
+      console.log(myInputField.validity);
     }
-    return myDIV;
+  });
+  return myInputField;
 }
 
-function createHTMLelement_TABLE(styleclass){
-    let myTable = document.createElement("table");
-    if (styleclass) {
-        myTable.classList = styleclass;
-    }
+function createHTMLelement_Button(
+  text,
+  fn,
+  styleclass = undefined,
+  img = undefined
+) {
+  const myButton = document.createElement("button");
+  myButton.textContent = text;
 
-    return myTable;
+  myButton.addEventListener("click", fn);
+  if (styleclass) {
+    myButton.classList += styleclass;
+  }
+
+  if (img) {
+    myButton.style.backgroundImage = `url(${img})`;
+  }
+  return myButton;
 }
 
-function createHTMLelement_TableCaption(captionText){
-    let myCaption = document.createElement("caption");
-    myCaption.innerText = captionText;
-    return myCaption;
-}
-
-function createHTMLelement_TableHeaders(array_of_headers = []){
-    let myTableHead = document.createElement("thead");
-    let myTableHeadRow = document.createElement("tr");
-    array_of_headers.forEach(header => {
-        let myTableHeadRowHeader = document.createElement("th");
-        myTableHeadRowHeader.innerText = header;
-        myTableHeadRow.appendChild(myTableHeadRowHeader)
-    })
-    myTableHead.appendChild(myTableHeadRow);
-    
-    return myTableHead;
-}
-
-function createHTMLelement_TableBody(){
-    let myTableBody = document.createElement("tbody");
-    return myTableBody;
-}
-
-function createHTMLelement_TableRow(array_of_data){
-    let myTableBodyRow = document.createElement("tr");
-    array_of_data.forEach(data => {
-        let myTableBodyRowData = document.createElement("td");
-        myTableBodyRowData.innerText = data;
-        myTableBodyRow.appendChild(myTableBodyRowData);
-    })
-    
-    return myTableBodyRow;
-}
-
-export { 
-    createHTMLelement_DIV, 
-    createHTMLelement_H, 
-    createHTMLelement_P, 
-    renderHTMLMenuObject,
-    createHTMLelement_TABLE,
-    createHTMLelement_TableCaption,
-    createHTMLelement_TableHeaders,
-    createHTMLelement_TableBody,
-    createHTMLelement_TableRow,
-    page_HTMLRenderer };
+export {
+  createHTMLelement_DIV,
+  createHTMLelement_H,
+  createHTMLelement_P,
+  createHTMLelement_label,
+  renderHTMLMenuObject,
+  createHTMLelement_TABLE,
+  createHTMLelement_TableCaption,
+  createHTMLelement_TableHeaders,
+  createHTMLelement_TableBody,
+  createHTMLelement_TableRow,
+  createHTMLelement_TableRowWithElements,
+  createHTMLelement_Form,
+  createHTMLelement_InputText,
+  createHTMLelement_Button,
+  page_HTMLRenderer,
+};
